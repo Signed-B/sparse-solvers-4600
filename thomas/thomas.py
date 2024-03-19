8ef6e6db9176e2e6397fb538051eb79aba86f32d
Beckett Hyde <beckett@becketthyde.com>
2024-03-19 10:56:06 -0600

    Starting work, not much

---
+++ thomas/thomas.py
@@ -0,0 +1,9 @@
+import numpy as np
+import scipy as sp
+
+def thomas(A: np.array, d: np.array, *args) -> np.array:
+    """
+    Solves a tridiagonal system of equations using the Thomas algorithm.
+    """
+    # print("Not implemented yet.")
+    pass
