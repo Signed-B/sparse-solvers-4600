60d906bab71db421f610cdd01a5aeb587c8e14f9
ronald-curry <rocu1744@colorado.edu>
2024-04-16 10:30:37 -0600

    Create poisson_1d.py

---
+++ poisson/poisson_1d.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib as plt
+
+
+a=0;
+b=5;
+n=10;
+
+
+f= lambda x: x**2 #forcing
+g= lambda x: x #exact
+
+
+
+def poisson_solve(a,b,n,f,g):
+    h=(b-a)/(n-1)
+    x=np.linspace(a,b,n)
+    A=np.zeros([n,n])
+    rhs=np.zeros([n,1])
+    i=0
+    A[i,i]=1
+    rhs[i]=g(x[i])
+    for i in range(1,n-1):
+        A[i,i-1]=-1.0/(h**2);
+        A[i,i]=2/(h**2);
+        A[i,i+1]=-1/(h**2);
+        rhs[i]=f(x[i]);
+    i=n-1
+    A[i,i]=1
+    rhs[i]=g(x[i])
+    u=np.linalg.solve(A, rhs)
+    return x,u,A
+        
+x,u,A=poisson_solve(a, b, n, f, g)
