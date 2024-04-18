b2ab646a5d4b5181d9acdbefa793bcbe9ad955d2
ronald-curry <rocu1744@colorado.edu>
2024-04-17 20:50:44 -0600

    2d

--- poisson/poisson_1d.py
+++ poisson/poisson_1d.py
@@ -1,22 +1,24 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
-a=0;
-b=5;
-n=10;
-
-
-f= lambda x: x**2 #forcing
-g= lambda x: x #exact
+a=-2
+b=2
+n=5
 
 
 
+f= lambda x: x #forcing
+g= lambda x: x**3 #exact for endpoints
+
+
+#u''(x)=f(x)
 def poisson_solve(a,b,n,f,g):
     #a-b interval
-    #number discrete point on interval
+    #n-number discrete point on interval
     #f forcing function
-    #exact for endpoints
+    #exact function to calc endpoints
+    #c_1, c_2 are coefficients on diff eq
     h=(b-a)/(n-1)
     x=np.linspace(a,b,n)
     A=np.zeros([n,n])
@@ -25,10 +27,10 @@ def poisson_solve(a,b,n,f,g):
     A[i,i]=1
     rhs[i]=g(x[i])
     for i in range(1,n-1):
-        A[i,i-1]=-1.0/(h**2);
-        A[i,i]=2/(h**2);
-        A[i,i+1]=-1/(h**2);
-        rhs[i]=f(x[i]);
+        A[i,i-1]=1.0/(h**2)
+        A[i,i]=-2/(h**2)
+        A[i,i+1]=1/(h**2)
+        rhs[i]=f(x[i])
     i=n-1
     A[i,i]=1
     rhs[i]=g(x[i])
@@ -36,3 +38,6 @@ def poisson_solve(a,b,n,f,g):
     return x,u,A
         
 x,u,A=poisson_solve(a, b, n, f, g)
+plt.plot(x, u)
+plt.plot(x,g(x))
+plt.show()
---
+++ poisson/poisson_2d.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from time import process_time
+
+n=10 #number points per direction
+x1=0
+x2=1
+y1=0
+y2=1
+
+
+
+def f(x,y): #forcing
+    return x*y+7
+
+def g(x,y): #end conditions function
+    return 4*x+y
+
+def poisson_solve(A,F): #solve linear system given banded matrix A and vecotr F
+    U=np.linalg.solve(A,F)
+    return U
+
+def poisson_setup(x1,x2,y1,y2,n,f,g): #2d finite difference setup
+    #square bounds only
+    xrange=np.linspace(x1,x2,n)
+    yrange=np.linspace(y1,y2,n)
+    n2=n
+    n=n**2
+    A=np.zeros([n,n])
+    F=np.zeros(n)
+    h=(x2-x1)/(n2)
+    A[0,0]=4
+    A[1,1]=4
+    A[2,2]=4
+    A[n-1,n-1]=4
+    A[n-2,n-2]=4
+    A[n-3,n-3]=4
+    A[1,0]=-1
+    A[0,1]=-1
+    A[2,1]=-1
+    A[1,2]=-1
+    A[2,3]=-1
+    A[0,3]=-1
+    A[1,4]=-1
+    A[2,5]=-1
+    A[n-1,n-2]=-1
+    A[n-2,n-1]=-1
+    A[n-2,n-3]=-1
+    A[n-3,n-2]=-1
+    A[n-3,n-4]=-1
+    A[n-1,n-4]=-1
+    A[n-2,n-5]=-1
+    A[n-3,n-6]=-1
+    for i in range(3,n-3):
+        A[i,i]=4
+        A[i,i+1]=-1
+        A[i,i+3]=-1
+        A[i,i-1]=-1
+        A[i,i-3]=-1
+    A=A*(1/(h**2))
+    for i in range(n2):
+        for j in range(n2):
+            F[i*n2+j]=f(xrange[i],yrange[j])
+
+    return A,F
+        
+A,F=poisson_setup(x1,x2,y1,y2,n,f,g)
+start_time=process_time()
+U=poisson_solve(A, F)
+end_time=process_time()
+run_time=end_time-start_time
+
+solution_field=np.zeros([n,n])
+for i in range(n):
+    for j in range(n):
+        solution_field[i,j]=F[i*n+j]
+    
+    
+    
+    
+    
+    
+    
+    
+    
