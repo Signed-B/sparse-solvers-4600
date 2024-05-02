d40742020b1c75da2120d9cc6073358477d07079
ronald-curry <rocu1744@colorado.edu>
2024-05-02 15:43:47 -0600

    ef

--- poisson/poisson2d.m
+++ poisson/poisson2d.m
@@ -1,6 +1,6 @@
 clc; clear; close all;
 
-n=6; %number points per direction
+n=5; %number points per direction
 
 x1=0; %bounrdries
 x2=1;
@@ -26,15 +26,13 @@ A=zeros(n^2,n^2); %creates empty variables
 rhs=zeros(n^2,1);
 
 for i=1:n^2
-    on_boundry=0; %boundry boolean
     if i<=n || i>n^2-n || mod(i,n)==0 || mod(i,n)==1  %checks if u_i is on boundry
-        on_boundry=1;
-        rhs(i)=g(D(i,1),D(i,2)); %if yes rhs is boundry function
-        A(i,i)=1; %corresponding row in A is just part of identity
+        rhs(i)=g(D(i,1),D(i,2)); %if yes, rhs is boundry function
+        A(i,i)=1; %corresponding row in A is just row of identity matrix
     else
-        rhs(i)=f(D(i,1),D(i,2)); %if not rhs is f
+        rhs(i)=f(D(i,1),D(i,2)); %if no, rhs is f
         for j=1:n^2
-            A(i,i)=-4/h^2; %approximates second derivvative
+            A(i,i)=-4/h^2; %approximates second derivvative in both directions at point
             A(i,i+1)=1/h^2;
             A(i,i-1)=1/h^2;
             A(i,i+(n))=1/h^2;
@@ -45,18 +43,19 @@ end
 
 U=A\rhs; %solves system
 
-grid_u=zeros(n,n); %converst solution back to matrix form
+grid_u=zeros(n,n); %converts solution back to matrix form
 for i=1:n
     for j=1:n
         grid_u(i,j)=U(n*(i-1)+j);
     end
 end
 hold on;
-surface(grid_x,grid_y,grid_u) %plots solution
+surface(grid_x,grid_y,grid_u,"FaceColor",'#0072BD',"FaceAlpha",1) %plots solution
 
 % calcs and plot actual
-[real_x,real_y]=meshgrid(x1:(x2-x1)/200:x2,y1:(x2-x1)/200:y2);
-surface(real_x,real_y,actual(real_x,real_y))
+[real_x,real_y]=meshgrid(x1:(x2-x1)/150:x2,y1:(x2-x1)/150:y2);
+surface(real_x,real_y,actual(real_x,real_y),"FaceColor",'r','EdgeColor', 'interp',"FaceAlpha",0.3)
+view(3)
 
 
 function [z]=f(x,y) %rhs forcing function
@@ -64,9 +63,9 @@ function [z]=f(x,y) %rhs forcing function
 end
 
 function [z]=g(x,y) %broundry condition function
-    z=0; % zero for dirichlet boundry conditions
+    z=x.*y; % zero for dirichlet boundry conditions
 end
 
 function [z]=actual(x,y) %actual solution to ivp (for plots and error)
-    z=sin(2*pi*x).*sin(2*pi*y);
+    z=sin(2*pi*x).*sin(2*pi*y)+x.*y;
 end
